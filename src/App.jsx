6d0096cabed8da68fa2692be1c137303dc875726
emilykohlberg <emilykohlberg@dhcp-10-105-103-186.wireless.northwestern.private>
2022-09-26 21:51:35 -0500

    make intial page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,17 @@ import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const schedule = {
+  title: "CS Courses for 2018-2019"
+};
+
 const App = () => {
-  const today = new Date();
-  const day = today.toLocaleString([], {weekday: 'long'});
-  const date = today.toLocaleDateString([], {dateStyle: 'long'})
+
+  return (
+    <div>
+      <h1>{schedule.title}</h1>
+    </div>
+  );
 };
 
 export default App;
